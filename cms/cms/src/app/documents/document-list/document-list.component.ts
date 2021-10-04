c16c534149f590d70557a9241498a734451fef91
BassettJake <35384947+BassettJake@users.noreply.github.com>
2021-10-04 17:39:13 -0600

    week04

--- cms/cms/src/app/documents/document-list/document-list.component.ts
+++ cms/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(1, "Doc1","The 1st document", '../../assets/images/jacksonk.jpg', []),
+    new Document(1, "Doc2","The 2nd document", '../../assets/images/jacksonk.jpg', []),
+    new Document(1, "Doc3","The 3rd document", '../../assets/images/jacksonk.jpg', []),
+    new Document(1, "Doc4","The 4th document", '../../assets/images/jacksonk.jpg', []),
+    new Document(1, "Doc5","The 5th document", '../../assets/images/jacksonk.jpg', [])
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
